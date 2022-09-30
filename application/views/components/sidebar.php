f43d7f499a531d1bb7ee8e8ac883f2af6d791d5e
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-30 08:40:32 -0300

    Ajustando sidebar

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -7,12 +7,14 @@
 		</form>
 	</div>
 	<div class="sidebar-box ftco-animate">
-		<h3 class="sidebar-heading">Categories</h3>
+		<h3 class="sidebar-heading">Categorias</h3>
 		<ul class="categories">
-			<li><a href="<?= base_url('blog/categoria/1')?>">Fashion <span>(6)</span></a></li>
-			<li><a href="<?= base_url()?>">Technology <span>(8)</span></a></li>
-			<li><a href="<?= base_url()?>">Travel <span>(2)</span></a></li>
-			<li><a href="<?= base_url()?>">Food <span>(2)</span></a></li>
-			<li><a href="<?= base_url()?>">Photography <span>(7)</span></a></li>
+			<?php if (count($categorias)) : ?>
+				<?php foreach ($categorias as $categoria) : ?>
+					<li><a href="<?= base_url('blog/categoria/' . $categoria->id) ?>"><?= $categoria->nome ?><span>(<?= $categoria->num_posts ?>)</span></a></li>
+				<?php endforeach  ?>
+			<?php else : ?>
+				<li><a href="<?= base_url() ?>">Não há categorias <span>(0)</span></a></li>
+			<?php endif ?>
 		</ul>
 	</div>
