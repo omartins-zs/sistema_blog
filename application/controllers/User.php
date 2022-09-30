1c0a23d356103580b3e73dd808ceeed59a5706d0
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-30 12:09:04 -0300

    Criando controller e ajustando view de Login

---
+++ application/controllers/User.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->model('Post_model');
+    //     $this->load->model('Categoria_model');
+    //     $this->load->helper('blog');
+    //     // $this->var = $var;
+    // }
+
+
+    // public function index()
+    // {
+    //     $data['categorias'] = $this->Categoria_model->get_categorias();
+    //     $data['posts'] = $this->Post_model->get_posts();
+    //     $data['subview'] = 'index';
+    //     $this->load->view('layout', $data);
+    // }
+
+    public function login()
+    {
+        // $data['categorias'] = $this->Categoria_model->get_categorias();
+        // $data['post'] = $this->Post_model->get($post_id);
+        // $data['subview'] = 'post';
+        $this->load->view('login_layout', $data);
+    }
+    // public function categoria($categoria_id)
+    // {
+    //     $data['posts'] = $this->Post_model->get_posts($categoria_id);
+    //     $data['categorias'] = $this->Categoria_model->get_categorias();
+    //     $data['subview'] = 'categoria';
+    //     $this->load->view('layout', $data);
+    // }
+
+    // public function sobre()
+    // {
+    //     $data['subview'] = 'sobre';
+    //     $this->load->view('pages_layout', $data);
+    // }
+    // public function contato()
+    // {
+    //     $data['subview'] = 'contato';
+    //     $this->load->view('pages_layout', $data);
+    // }
+}
--- application/views/login_layout.php
+++ application/views/login_layout.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url() ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet"> 
+    <link href="<?= base_url() ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -29,18 +29,20 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-6 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <!-- Aqui volta a imagem -->
+                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
+
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
@@ -51,23 +53,25 @@
                                             <input type="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Password">
                                         </div>
-                                        <div class="form-group">
+
+                                        <!-- Lembrar-me --> 
+                                      <!-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember
                                                     Me</label>
                                             </div>
-                                        </div>
+                                        </div> -->
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        </a> -->
                                     </form>
                                     <hr>
                                     <div class="text-center">
