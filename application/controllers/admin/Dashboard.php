ca8f178c2364c1420ce6fe94599caa2953dab84f
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-11 08:52:11 -0300

    Criando Controller para o Dashboard

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,21 +3,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     $this->load->model('Post_model');
-    //     $this->load->model('Categoria_model');
-    //     $this->load->helper('blog');
-    //     // $this->var = $var;
-    // }
-
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        //$this->session->userdata('loggedin') == TRUE || redirect('user/login');
+    }
 
     public function index()
     {
-        // $data['categorias'] = $this->Categoria_model->get_categorias();
-        // $data['posts'] = $this->Post_model->get_posts();
-        $data['subview'] = 'admin/index';
-        $this->load->view('admin/main_layout', $data);
+        $this->data['subview'] = 'admin/index';
+        $this->load->view('admin/main_layout', $this->data);
     }
 }
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/admin/Dashboard.php */
