9f1b113fb70f307634ede88e8597a191a3851158
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-11 09:01:06 -0300

    Criado view para editar os Posts

--- application/views/admin/post/editar.php
+++ application/views/admin/post/editar.php
@@ -1,45 +1,63 @@
 <div class="card shadow mb-4">
-    <div class="card-header py-3"></div>
+    <div class="card-header py-3"><?= empty($post->titulo) ? 'Novo artigo' : 'Editar artigo'; ?> </div>
     <div class="card-body">
-        <form>
-            <div class="form-group">
-                <label class="small mb-1" for="inputUsername">Upload da imagem</label>
-                <img src="..." id="img_url" alt="Sua imagem" class="img-thumbnail">
-                <input class="form-control-file mt-2" id="inputUsername" type="file" onchange="img_pathUrl(this)">
+        <?php if (validation_errors()) { ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?= validation_errors('<li>', '</li>'); ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="form-group">
-                <label class="small mb-1" for="inputUsername">Titulo</label>
-                <input class="form-control" id="inputUsername" type="text" value="">
+        <?php } ?>
+        <?php if ($this->session->flashdata('error_msg')) : ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?= $this->session->flashdata('error_msg') ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="form-group">
-                <label class="small mb-1" for="exampleFormControlTextarea1">Descrição</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <div class="form-group">
-                <label class="small mb-1" for="exampleFormControlSelect2">Selecioanar categoria</label>
-                <select class="form-control" id="exampleFormControlSelect2">
-                    <option>1</option>
-                    <option>1</option>
-                    <option>1</option>
-                    <option>1</option>
-                    <option>1</option>
-                </select>
-            </div>
-            <button class="btn btn-primary" type="submit">Registrar post</button>
-        </form>
+        <?php endif ?>
+        <!-- <?php if (!empty($error_msg)) : ?>
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <?= $error_msg ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+        <?php endif ?> -->
+        <?= form_open_multipart(); ?>
+        <div class="form-group">
+            <label class="small mb-1" for="inputUsername">Upload da imagem</label>
+
+            <?php if ($post->imagem == '') : ?>
+                <img src="<?= base_url('assets/images/default.jpg'); ?>" id="img_url" alt="tu imagen" class="img-thumbnail">
+            <?php else : ?>
+                <img src="<?= base_url('assets/images/' . $post->imagem); ?>" id="img_url" alt="tu imagen" class="img-thumbnail">
+            <?php endif; ?>
+
+            <input type="hidden" name="imagem_nome" value="<?= $post->imagem ?>">
+            <input class="form-control-file mt-2" id="inputUsername" type="file" name="imagem" onChange="img_pathUrl(this);">
+        </div>
+        <div class="form-group">
+            <label class="small mb-1" for="inputUsername">Titulo</label>
+            <input class="form-control" id="inputUsername" type="text" name="titulo" value="<?= set_value('nome', $this->input->post('titulo') ? $this->input->post('titulo') : $post->titulo); ?>">
+        </div>
+        <div class="form-group">
+            <label class="small mb-1" for="exampleFormControlTextarea1">Descrição</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao"><?= set_value('descricao', $post->descricao); ?></textarea>
+        </div>
+        <div class="form-group">
+            <label class="small mb-1" for="exampleFormControlSelect2">Selecionar Categoria</label>
+            <select class="form-control" id="exampleFormControlSelect2" name="categoria_id">
+                <?php if ($post->categoria_id == 0) : ?>
+                    <option value="" selected>Selecionar categoria</option>
+                <?php endif ?>
+                <?php foreach ($categorias as $categoria) : ?>
+                    <option value="<?= $categoria->id ?>" <?php if ($categoria->id == $post->categoria_id) echo "selected" ?> <?= set_select('categoria_id', $categoria->id) ?>><?= $categoria->nome ?></option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <button class="btn btn-primary" type="submit">Registrar Articulo</button>
+        <?php echo form_close() ?>
     </div>
 </div>
-<!-- 
-            <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
-    </div>
-    <div class="form-group row">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-        </div>
-        <div class="col-sm-6">
-            <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
-        </div>
-    </div>
-    <a href="login.html" class="btn btn-primary btn-user btn-block">
-        Register Account
-    </a> -->
