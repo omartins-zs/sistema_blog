a8fdce5b52b0794497e934dd6142e08fbf897d44
omartins-zs <gabrielmatheus209@gmail.com>
2026-08-11 08:57:05 -0300

    :bug: fix: corrigir GROUP BY para compatibilidade MySQL 5.7

--- application/models/Categoria_model.php
+++ application/models/Categoria_model.php
@@ -28,7 +28,7 @@ class Categoria_model extends MY_Model
         $this->db->select('c.id, c.nome, count(*) as num_posts');
         $this->db->from('posts p');
         $this->db->join('categorias c', 'c.id = p.categoria_id', 'left');
-        $this->db->group_by('nome');
+        $this->db->group_by('c.id, c.nome');
 
         return $this->db->get()->result();
     }
