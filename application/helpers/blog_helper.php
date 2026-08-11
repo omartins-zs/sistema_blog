26405097129b4e628e958b1a97c52c1919ced175
omartins-zs <gabrielmatheus209@gmail.com>
2026-08-11 15:13:22 -0300

    :globe_with_meridians: i18n: corrigir traducao de datas independentemente do SO (removido strftime)

--- application/helpers/blog_helper.php
+++ application/helpers/blog_helper.php
@@ -1,30 +1,48 @@
-<?php
+ï»¿<?php
 
 function format_date($date)
 {
-    setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-    date_default_timezone_set('America/Sao_Paulo');
-
-    // Só a Data 
-    // return strftime('%d/%m/%Y', strtotime($date));
-
-    // DATA POR EXTENSO
-    return strftime('%A , %d de %B de %Y', strtotime($date));
+    $timestamp = strtotime($date);
+    
+    $dias = [
+        'Sunday' => 'domingo',
+        'Monday' => 'segunda-feira',
+        'Tuesday' => 'terÃ§a-feira',
+        'Wednesday' => 'quarta-feira',
+        'Thursday' => 'quinta-feira',
+        'Friday' => 'sexta-feira',
+        'Saturday' => 'sÃ¡bado'
+    ];
+    
+    $meses = [
+        'January' => 'janeiro',
+        'February' => 'fevereiro',
+        'March' => 'marÃ§o',
+        'April' => 'abril',
+        'May' => 'maio',
+        'June' => 'junho',
+        'July' => 'julho',
+        'August' => 'agosto',
+        'September' => 'setembro',
+        'October' => 'outubro',
+        'November' => 'novembro',
+        'December' => 'dezembro'
+    ];
+    
+    $dia_semana = $dias[date('l', $timestamp)];
+    $dia = date('d', $timestamp);
+    $mes = $meses[date('F', $timestamp)];
+    $ano = date('Y', $timestamp);
+    
+    return "{$dia_semana}, {$dia} de {$mes} de {$ano}";
 }
 
-// num words = número de palavras
-// limit num words = limite de palavras
 function limitePalavras($string, $numwords)
 {
-
     $excerpt = explode(' ', $string, $numwords + 1);
-
     if (count($excerpt) >= $numwords) {
-        // array_pop — Extrai um elemento do final do array
         array_pop($excerpt);
     }
-
     $excerpt = implode(' ', $excerpt);
-
     return $excerpt;
 }
